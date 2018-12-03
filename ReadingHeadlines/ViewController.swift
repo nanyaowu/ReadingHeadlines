e3d6c1cd5289c5dc486c3849a2ec2db07b4f8a8b
Nan-Yao Wu <nanyao.h.wu@gmail.com>
2018-12-03 12:35:49 +0800

    Initial Commit

---
+++ ReadingHeadlines/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadingHeadlines
+//
+//  Created by freakytune on 2018/12/3.
+//  Copyright © 2018 freakytune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
