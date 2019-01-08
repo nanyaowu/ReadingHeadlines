2552c17813ed2bcafb129b22b50d2f111c604566
Nan-Yao Wu <nanyao.h.wu@gmail.com>
2019-01-08 22:43:10 +0800

    no use

--- ReadingHeadlines/FeedsViewController.swift
+++ ReadingHeadlines/FeedsViewController.swift
@@ -12,7 +12,8 @@ import UIKit
 class FeedsViewController: UITableViewController {
     
     var newsItemStore: NewsItemStore!
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
